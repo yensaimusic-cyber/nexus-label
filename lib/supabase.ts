c3212712815ec05c836ca517c21249bfa240ab2e
yensaimusic-cyber <yensai.music@gmail.com>
2026-02-10 04:21:07 +0100

    Update supabase.ts
    new

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-// Remplacer ces valeurs par tes propres clés Supabase une fois le projet créé
-const supabaseUrl = 'https://uphtjvuacznfmosb_publishable_YabzV160QA383MBQOS6d4A_wJesuH8yta-cle-anonyme';
+// Les variables d'environnement sont automatiquement chargées depuis Netlify
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
